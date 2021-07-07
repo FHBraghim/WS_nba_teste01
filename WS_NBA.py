abdb78e0b285984aa42f78b8ee78bb560ce0ed31
FHBraghim <65635249+FHBraghim@users.noreply.github.com>
2021-07-06 21:20:40 -0300

    Update WS_NBA.py
    Corrigindo bug do gráfico.

--- WS_NBA.py
+++ WS_NBA.py
@@ -1,22 +1,11 @@
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-req = requests.get('https://www.basketball-reference.com/leagues/NBA_2018_totals.html')
 
-if req.status_code == 200:
-    print('Requisição bem sucedida!')
-    content = req.content
-
-#   Criando objeto e acessando elementos chamando o método find.
-soup = BeautifulSoup(content,'html.parser')
-table = soup.find(name='table')
-
-table_str = str(table)
-df = pd.read_html(table_str)
-
-# Loop com range para iterar estatísticas de 2013 a 2018
-
+# Método para extrair várias páginas de uma só vez
 def scrape_stats(base_url, year_start, year_end):
     years = range(year_start,year_end+1,1)
 
@@ -32,33 +21,31 @@ def scrape_stats(base_url, year_start, year_end):
         df['Year'] = year
 
         final_df = final_df.append(df)
-
-        return final_df
-
-    url = 'https://www.basketball-reference.com/leagues/NBA_{}_totals.html'
-
-    df = scrape_stats(url, 2013,2018)
+    return final_df
 
 
+# Utilizando o método
+url = 'https://www.basketball-reference.com/leagues/NBA_{}_totals.html'
+df = scrape_stats(url, 2013,2018)
 
+drop_indexes = df[df['Rk'] == 'Rk'].index # Pega indexes onde a coluna 'RK' possui valor 'RK'.
+df.drop(drop_indexes, inplace=True) # Elimina os valores dos index passados da tabela
 
+# Convertendo tabelas para valores numéricos
+numeric_cols = df.columns.drop(['Player', 'Pos', 'Tm'])
+df[numeric_cols] = df[numeric_cols].apply(pd.to_numeric)
 
+# Gráfico de média de bolas de 3 pontos arremessadas por ano
+plt.scatter(x='Year', y='3PA')
+plt.show()
 
+sorted_df = df.sort_values(by=['3P'], axis=0, ascending=False)
+sorted_df[['Player', '3P', 'Year']].head()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# Agrupando os dados por jogador e somando os valores
+grouped_df = df.groupby('Player', as_index=False).sum()
+# Ordena Data Frame por bolas de 3 pontos convertidas em ordem decrescente
+sorted_df = df.sort_values(by=['3P'], axis=0, ascending=False)
+# Mostra 5 primeiras posições da tabela
+sorted_df[['Player', '3P', '3PA']].head()
+print(sorted_df)
